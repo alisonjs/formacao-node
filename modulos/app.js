eb3e0eb65bde2a032fe4b4c9c9f7ec96081fdd81
Alison Silva <alison.dev.silva@gmail.com>
2021-02-13 00:27:02 -0300

    Modules

---
+++ modulos/app.js
@@ -0,0 +1,6 @@
+const calculadora = require('./calculadora');
+
+console.log(calculadora.soma(10, 20));
+console.log(calculadora.div(10, 20));
+console.log(calculadora.mult(10, 20));
+console.log(calculadora.sub(10, 20));

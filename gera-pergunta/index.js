3c5a94f95bbf7e295a8d6368ee2617b1ac95c1f8
Alison Silva <alison.dev.silva@gmail.com>
2021-02-15 20:43:33 -0300

    Add dynamic variable to html files

--- gera-pergunta/index.js
+++ gera-pergunta/index.js
@@ -5,8 +5,37 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/:name/:lang', (req, res) => {
+  const name = req.params.name;
+  const lang = req.params.lang;
+  const msg = false;
+  const products = [
+    { name: 'Cereal', price: 5.69 },
+    { name: 'Meat', price: 56 },
+    { name: 'Watter', price: 4 },
+    { name: 'Mint', price: 4 },
+    { name: 'Banana', price: 8 },
+    { name: 'Nuts', price: 12 },
+  ];
+
+  res.render('index', {
+    name,
+    lang,
+    company: 'Uniquati',
+    subscribers: 8000,
+    msg,
+    products,
+  });
 });
 
-app.listen(3000);
+app.get('/home', (req, res) => {
+  res.render('home');
+});
+
+app.get('/principal/profile', (req, res) => {
+  res.render('principal/profile');
+});
+
+app.listen(3000, () => {
+  console.log('App running');
+});

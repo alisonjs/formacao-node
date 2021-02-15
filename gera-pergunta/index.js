ec64ee7cc00a670bb1b6436aad167dfdd716c410
Alison Silva <alison.dev.silva@gmail.com>
2021-02-15 16:53:22 -0300

    Initial commit - Gera Pergunta

---
+++ gera-pergunta/index.js
@@ -0,0 +1,12 @@
+const { response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000);
